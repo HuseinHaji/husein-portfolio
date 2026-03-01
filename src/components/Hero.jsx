d02bb59216ef7b5c3e6af310c8c8f4e15f959ca5
HuseinHaji <huseyn1hajiyev@gmail.com>
2026-03-01 19:22:11 +0100

    Change header photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import huseynPhoto from "../assets/huseyn.jpeg";
+import huseynPhoto from "../assets/huseyn.png";
 
 export default function Hero() {
   return (
